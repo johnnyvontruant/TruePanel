c94bf75e45ef7e9251c39b5c7817cdbf0e49d3a2
root <root@BattleStation>
2026-07-02 13:06:41 -0700

    feat: add SMART watcher

--- mission_control.py
+++ mission_control.py
@@ -3,6 +3,7 @@ from truepanel.mission_control.watchers.healthy import healthy_watcher
 from truepanel.mission_control.watchers.pool import pool_watcher
 from truepanel.mission_control.watchers.thermal import thermal_watcher
 from truepanel.mission_control.watchers.zfs import zfs_watcher
+from truepanel.mission_control.watchers.smart import smart_watcher
 
 
 if __name__ == "__main__":
@@ -11,6 +12,7 @@ if __name__ == "__main__":
     mission.register(pool_watcher)
     mission.register(thermal_watcher)
     mission.register(zfs_watcher)
+    mission.register(smart_watcher)
     mission.register(healthy_watcher)
 
     event = mission.evaluate({
@@ -27,7 +29,19 @@ if __name__ == "__main__":
             "percent": 42,
             "remaining": None,
             "problem": False,
-        }
+        },
+        "smart": [
+            {
+                "drive": "sdb",
+                "health": "PASSED",
+                "reallocated": 11144,
+                "pending": 160,
+                "offline_uncorrectable": 160,
+                "reported_uncorrect": 900,
+                "media_errors": 0,
+                "critical_warning": "0x00",
+            }
+        ]
     })
 
     print(event)
