54561acf41e5d23d7a40b81d6e45a285130e8c38
johnnyvontruant <50461284+johnnyvontruant@users.noreply.github.com>
2026-08-21 12:45:34 -0700

    Export Lifeline replacement discovery

--- truepanel/lifeline/__init__.py
+++ truepanel/lifeline/__init__.py
@@ -7,6 +7,7 @@ from .profiles import (
     service_profile,
     service_profile_for_config,
 )
+from .replacement import ReplacementCandidateProvider, parse_block_signatures
 from .runtime import attach_repair_sessions
 from .session import (
     DRIVE_PHASES,
@@ -25,9 +26,11 @@ __all__ = [
     "RepairGate",
     "RepairSession",
     "ReplacementAssessment",
+    "ReplacementCandidateProvider",
     "ServiceProfile",
     "attach_repair_sessions",
     "evaluate_drive_repair",
+    "parse_block_signatures",
     "profile_keys",
     "service_profile",
     "service_profile_for_config",
