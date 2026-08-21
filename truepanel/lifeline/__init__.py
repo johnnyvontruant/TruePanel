de9e96bf2d8b7b70cfc717b6c7582b59dc68be8b
johnnyvontruant <50461284+johnnyvontruant@users.noreply.github.com>
2026-08-21 12:35:48 -0700

    Export Lifeline service profile registry

--- truepanel/lifeline/__init__.py
+++ truepanel/lifeline/__init__.py
@@ -1,5 +1,12 @@
 """Project Lifeline guided-repair primitives."""
 
+from .profiles import (
+    QNAP_TVS_X71,
+    ServiceProfile,
+    profile_keys,
+    service_profile,
+    service_profile_for_config,
+)
 from .runtime import attach_repair_sessions
 from .session import (
     DRIVE_PHASES,
@@ -14,9 +21,14 @@ __all__ = [
     "DEFAULT_LIFELINE_PATH",
     "DRIVE_PHASES",
     "LifelineSessionStore",
+    "QNAP_TVS_X71",
     "RepairGate",
     "RepairSession",
     "ReplacementAssessment",
+    "ServiceProfile",
     "attach_repair_sessions",
     "evaluate_drive_repair",
+    "profile_keys",
+    "service_profile",
+    "service_profile_for_config",
 ]
